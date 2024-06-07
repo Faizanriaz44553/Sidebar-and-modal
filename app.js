c7a833305d218985d87f995588141a9bd1186f61
Faizanriaz44553 <sylanipractice@gmail.com>
2024-06-07 05:45:34 -0700

    project initialization

---
+++ app.js
@@ -0,0 +1,8 @@
+let navbar = document.querySelector("#navbar")
+let modal = document.querySelector("#modal")
+function navbaropen() {
+    navbar.classList.toggle('open')
+}
+function modalopen() {
+    modal.classList.toggle('modaljs')
+}
